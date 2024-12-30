3753fa3192343f462190f98d9a4861664727615b
KonstantinNoskov <noskovjr@yandex.ru>
2024-12-31 01:04:19 +0500

    1. Setup BasePLayerController 2. Some assets for brute

---
+++ Source/Brute/Private/Game/Controllers/BasePlayerController.cpp
@@ -0,0 +1,54 @@
+﻿#include "Brute/Public/Game/Controllers/BasePlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+ABasePlayerController::ABasePlayerController()
+{
+	
+}
+
+void ABasePlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Mapping context should be set
+	check(BruteContext)
+	
+	UEnhancedInputLocalPlayerSubsystem* InputSubSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(InputSubSystem)
+
+	// Set Brute's mapping context
+	InputSubSystem->AddMappingContext(BruteContext, 0);
+}
+
+void ABasePlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	// EnhancedInputComponent Valid Check
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABasePlayerController::Move);
+	
+}
+
+void ABasePlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+	
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+		
+	}
+}
+
+
---
+++ Source/Brute/Public/Game/Controllers/BasePlayerController.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BasePlayerController.generated.h"
+
+
+struct FInputActionValue;
+class UInputAction;
+class UInputMappingContext;
+
+UCLASS()
+class BRUTE_API ABasePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	
+	ABasePlayerController();
+	
+protected:
+	
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+private:
+
+	void Move(const FInputActionValue& InputActionValue);
+	
+	UPROPERTY(EditAnywhere, Category = "input")
+	TObjectPtr<UInputMappingContext> BruteContext;
+
+	UPROPERTY(EditAnywhere, Category = "input")
+	TObjectPtr<UInputAction> MoveAction;
+	
+	
+};
