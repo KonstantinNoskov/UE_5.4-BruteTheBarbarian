eaa9a46f716a1a88c04f8259f46fdd630ebcf184
KonstantinNoskov <noskovjr@yandex.ru>
2024-12-30 22:30:01 +0500

    Init Commit

---
+++ Source/Brute/Public/Characters/BruteCharacter.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerCharacter.h"
+#include "BruteCharacter.generated.h"
+
+UCLASS()
+class BRUTE_API ABruteCharacter : public APlayerCharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABruteCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	
+	virtual void Tick(float DeltaTime) override;
+	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
---
+++ Source/Brute/Public/Game/GameMode/BruteGameMode.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BruteGameMode.generated.h"
+
+UCLASS()
+class BRUTE_API ABruteGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+};
